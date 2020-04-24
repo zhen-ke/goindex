67bf64381171afaed261e9474df7ce2e0cea8313
xmit <xmit@xmit-MBP.lan>
2020-04-24 16:50:03 +0800

    fix-4

--- themes/material/app.js
+++ themes/material/app.js
@@ -21,7 +21,7 @@ document.write(
   '<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>'
 );
 document.write(
-  "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>"
+  "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}},.video-wrap{max-height: 500px;},</style>"
 );
 
 // 初始化页面，并载入必要资源
@@ -294,7 +294,9 @@ function file_video(path) {
   var content = `
 <div class="mdui-container-fluid">
 	<br>
-	<div id="dplayer"></div>
+	<div class="video-wrap">
+		div id="dplayer"></div>
+	</div>
 	<br>${playBtn}
 	<!-- 固定标签 -->
 	<div class="mdui-textfield">
@@ -345,9 +347,10 @@ function file_audio(path) {
     container: document.getElementById('aplayer'),
     audio: [{
         name,
-        url,
+				url,
+				cover: ""
     }]
-});
+	});
 }
 
 // 图片展示
